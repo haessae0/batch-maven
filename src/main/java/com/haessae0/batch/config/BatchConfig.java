36ef32bf43bde60eaa4906545436240e4c2a1e1a
haessae0 <haessae0@gmail.com>
2024-12-17 14:50:32 +0900

    기존 데이터 삭제 및 진짜 소스 시작 예정

---
+++ src/main/java/com/haessae0/batch/config/BatchConfig.java
@@ -0,0 +1,9 @@
+package com.haessae0.batch.config;
+
+import org.springframework.batch.core.configuration.annotation.EnableBatchProcessing;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+@EnableBatchProcessing
+public class BatchConfig {
+}
--- src/test/java/com/haessae0/batch/helloWorldBatch/HelloWorldTaskletTest.java
+++ src/test/java/com/haessae0/batch/helloWorldBatch/HelloWorldTaskletTest.java
@@ -1,13 +1,7 @@
 package com.haessae0.batch.helloWorldBatch;
 
-import static org.assertj.core.api.Assertions.assertThat;
-
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
-import org.springframework.batch.core.step.tasklet.Tasklet;
-import org.springframework.batch.repeat.RepeatStatus;
-
-import com.haessae0.batch.hello.tasklet.HelloWorldTasklet;
 
 class HelloWorldTaskletTest {
 
@@ -15,12 +9,12 @@ class HelloWorldTaskletTest {
 	@DisplayName("헬로_월드_테스클랫_테스트")
 	void testHelloWorldTasklet() throws Exception {
 		// given: Tasklet 인스턴스 생성
-		Tasklet tasklet = new HelloWorldTasklet();
+//		Tasklet tasklet = new HelloWorldTasklet();
 
 		// when: Tasklet 실행
-		RepeatStatus status = tasklet.execute(null, null);
+//		RepeatStatus status = tasklet.execute(null, null);
 
 		// then: 결과 검증
-		assertThat(status).isEqualTo(RepeatStatus.FINISHED);
+//		assertThat(status).isEqualTo(RepeatStatus.FINISHED);
 	}
 }
